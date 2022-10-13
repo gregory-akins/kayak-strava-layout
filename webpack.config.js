7de4f57a01158fb669dcdc00bd49cf0d77c2b6cc
Gregory P. Akins <gregory.akins@icf.com>
2022-10-13 14:22:33 -0400

    Enabling different URLs based on environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 const Dotenv = require("dotenv-webpack");
+const path = require("path");
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
@@ -13,9 +14,15 @@ module.exports = (webpackConfigEnv, argv) => {
   return merge(defaultConfig, {
     plugins: [
       new Dotenv({
+<<<<<<< Updated upstream
         path: `./.env${
           webpackConfigEnv.development === true ? ".development" : ""
         }`,
+=======
+        path: path.resolve(__dirname, `${
+          webpackConfigEnv.development  ? ".env.development" : ".env"
+        }`),
+>>>>>>> Stashed changes
       }),
     ],
   });
