e5da3de18e37281f242df08fcc234209c1af57c0
Gregory P. Akins <gregory.akins@icf.com>
2022-11-20 19:24:53 -0500

    Cleaning up my async/await - then gibberish

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,13 @@
-import { useState } from "react";
-import { useSessionStorage } from "usehooks-ts";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 
-import Button from "@mui/material/Button";
 import MenuIcon from "@mui/icons-material/Menu";
 
-import { useNavigate } from "react-router-dom";
-import { navigateToUrl } from "single-spa";
-import { useServiceConfig } from "@akinsgre/kayak-strava-utility";
-import { setUser } from "../actions";
+import StravaRedirect from "../StravaRedirect";
 
 export const Navbar = () => {
-  let clientId = "";
-  let redirectUrl: string = "";
-  useServiceConfig().then((value) => {
-    clientId = value.clientId;
-    redirectUrl = value.redirectUrl;
-  });
-
-  const [userName, setUserName] = useSessionStorage("userName", "Signup");
-  const navigate = useNavigate();
-
-  const handleLogin = () => {
-    const loginUrl = `http://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&approval_prompt=force&scope=activity:read_all&redirect_uri=${redirectUrl}/exchange_token`;
-
-    navigateToUrl(loginUrl);
-  };
-
   return (
     <AppBar position="static">
       <Toolbar>
@@ -41,12 +19,11 @@ export const Navbar = () => {
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography variant="h1" component="div" sx={{ flexGrow: 1 }}>
           Paddle Strava
         </Typography>
-        <Button color="inherit" onClick={handleLogin}>
-          {userName || "Signup"}
-        </Button>
+
+        <StravaRedirect setUser={undefined} setUserActivities={undefined} />
       </Toolbar>
     </AppBar>
   );
