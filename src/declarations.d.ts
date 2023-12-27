49858c8ad9543688df0ca5042eba658e0e94f3b4
Greg Akins <gregory.akins@icf.com>
2023-12-27 09:04:37 -0500

    Just updating declarations and mocks because of other components changing

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -42,15 +42,11 @@ declare module "@akinsgre/kayak-strava-utility" {
   export interface ServiceConfig {
     stravaUrl: string;
     clientId: string;
-    clientSecret: string;
     redirectUrl: string;
   }
   export function useServiceConfig(): Promise<ServiceConfig>;
-  export function authenticate(
-    REACT_APP_CLIENT_ID: string,
-    REACT_APP_CLIENT_SECRET: string
-  ): Athlete;
-  export function refreshAuth(): Promise<Token>;
+  export function authenticate(): Athlete;
+  export function refreshStravaAuth(): Promise<Token>;
   export function getAthlete(accessToken: String): Promise<Athlete>;
   export interface Token {
     access_token: string;
